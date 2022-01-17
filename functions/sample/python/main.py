043efc4ebb6b932a485658e51a500846d3bd5d13
Dogan-87 <doqanonay@gmail.com>
2022-01-17 15:23:19 +0000

    Temporary changes

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -6,22 +6,29 @@
 #
 # @return The output of this action, which must be a JSON object.
 #
-#
+# To return NAMES of ALL DATABASEs 
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 import requests
 
+secret={
+    "COUCH_URL": "https://45c14cd9-04bc-467b-a5fc-65c842636775-bluemix.cloudantnosqldb.appdomain.cloud",
+    "IAM_API_KEY": "-lTAU8tc-rHV1d0JQqwjHTyD7SYClm4nPLWPG7yML22F",
+    "COUCH_USERNAME": "45c14cd9-04bc-467b-a5fc-65c842636775-bluemix"
+}
 
 def main(dict):
     databaseName = "dealerships"
+    
 
     try:
         client = Cloudant.iam(
-            account_name=dict["COUCH_USERNAME"],
-            api_key=dict["IAM_API_KEY"],
+            account_name=secret["COUCH_USERNAME"],
+            api_key=secret["IAM_API_KEY"],
             connect=True,
         )
         print("Databases: {0}".format(client.all_dbs()))
+
     except CloudantException as ce:
         print("unable to connect")
         return {"error": ce}
