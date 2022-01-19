683774e233371906aadaba1c1efa3f4841da93d9
Dogan-87 <doqanonay@gmail.com>
2022-01-19 17:22:07 +0000

    Temporary changes

--- functions/sample/python/get_filtered_reviews.py
+++ functions/sample/python/get_filtered_reviews.py
@@ -6,7 +6,7 @@ import requests
 
 secret={
     "COUCH_URL": "https://45c14cd9-04bc-467b-a5fc-65c842636775-bluemix.cloudantnosqldb.appdomain.cloud",
-    "IAM_API_KEY": "-lTAU8tc-",
+    "IAM_API_KEY": "-lTAU8tc-rHV1d0JQqwjHTyD7SYClm4nPLWPG7yML22F",
     "COUCH_USERNAME": "45c14cd9-04bc-467b-a5fc-65c842636775-bluemix"
 }
 
@@ -19,39 +19,43 @@ def main(dict):
         )  
         
     my_database = client["reviews"]
-
-
+    
     try:
-
-        if(dict["id"]):
+        if "id" in dict :
             selector = {'id': {'$eq': int(dict["id"])}}
             result_by_filter = my_database.get_query_result(selector,raw_result=True)
-            result= {
-                'headers':{'Content-Type':'application/json'},
-                'body':{'data':result_by_filter}
+            print("sadece bir id")
 
-            }
-            return result
-
-        if(dict["dealership"]):
+        elif "dealership" in dict :
             selector = {'dealership': {'$eq': int(dict["dealership"])}}
             result_by_filter = my_database.get_query_result(selector,raw_result=True)
-            result= {
-                'headers':{'Content-Type':'application/json'},
-                'body':{'data':result_by_filter}
+            print("sadece bir dealership")
 
-            }
-            return result
+        else:
+            selector = {'id': {'$exists': True}}
+            result_by_filter = my_database.get_query_result(selector,raw_result=True)
+            print("hepsiiiiiiii")
+
+        result= {
+            'headers':{'Content-Type':'application/json'},
+            'body':{'data':result_by_filter}
+        }
+        return result["body"]["data"]["docs"][:] 
 
     except:
-        print("111111111111111111")
         return {
-                'statusCode': 404,
-                'message':"Something went wrong"
-            }
+               'statusCode': 404,
+              'message':"Something went wrong"
+          }    
 
+reviews_by_all= main({})
+print(reviews_by_all)
 
-#reviews_by_id= main({"id":"3"})
+#reviews_by_id= main({"id":"4"})
 #print(reviews_by_id)
-reviews_by_dealership= main({"dealership":"46"})
-print(reviews_by_dealership)
+
+#reviews_by_dealership= main({"dealership":"15"})
+#print(reviews_by_dealership)
+
+#reviews_by_new= main({"name":"4"})
+#print(reviews_by_new)
---
+++ functions/sample/python/post_review.py
@@ -0,0 +1,36 @@
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+import requests 
+
+secret={
+    "COUCH_URL": "https://45c14cd9-04bc-467b-a5fc-65c842636775-bluemix.cloudantnosqldb.appdomain.cloud",
+    "IAM_API_KEY": "-lTAU8tc-rHV1d0JQqwjHTyD7SYClm4nPLWPG7yML22F",
+    "COUCH_USERNAME": "45c14cd9-04bc-467b-a5fc-65c842636775-bluemix"
+}
+
+def main(dict):
+    databaseName = "reviews"
+
+    try:
+        client = Cloudant.iam(
+        account_name=secret["COUCH_USERNAME"],
+        api_key=secret["IAM_API_KEY"],
+        connect=True,
+        ) 
+
+def post_request(url, json_payload, **kwargs):
+
+    json_data = json.dumps(json_payload, indent=4)
+    print(f"{json_data}")
+
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.post(url, params=kwargs, json=json_data)
+
+    except Exception as e:
+        # If any error occurs
+        print("Network exception occurred")
+        print(f"Exception: {e}")
+
+    print(f"With status {response.status_code}")
+    print(f"Response: {response.text}")
